ea19ceca30b8cbdf50c12e590d5f190af1a40ab9
ermakov-oleg <ermakovolegs@gmail.com>
2026-01-17 12:35:18 +0100

    Add context module with Request, Context, StaticContext

--- lib/runtime-settings/src/lib.rs
+++ lib/runtime-settings/src/lib.rs
@@ -1,6 +1,8 @@
 // lib/runtime-settings/src/lib.rs
+pub mod context;
 pub mod entities;
 pub mod error;
 
+pub use context::{Context, Request, StaticContext};
 pub use entities::{McsResponse, Setting, SettingKey};
 pub use error::SettingsError;
