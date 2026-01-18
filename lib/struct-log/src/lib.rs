35e61230e81f2385ffa2f73d7812e07263165419
ermakov-oleg <ermakovolegs@gmail.com>
2026-01-18 23:28:43 +0100

    feat(struct-log): add StructLogBuilder for flexible configuration

--- lib/struct-log/src/lib.rs
+++ lib/struct-log/src/lib.rs
@@ -1,8 +1,10 @@
+mod builder;
 mod error;
 mod formatting_layer;
 mod setup;
 mod storage;
 
+pub use builder::StructLogBuilder;
 pub use error::SetupError;
 pub use formatting_layer::JsonLogLayer;
 pub use setup::setup_logger;
